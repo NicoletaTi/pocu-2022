e45d6675ed1a93fdcd4b49d7cdec179af8ba6ab0
Turc <nicoletaturc06@yahoo.com>
2022-08-04 09:20:27 +0200

    array

---
+++ cv/Experienta.js
@@ -0,0 +1,40 @@
+const experienta1= [
+    {
+        id: "1",
+        name: "Practica",
+        company: "Endava" ,
+        description: "Front-end development",
+        startDate: "01/07/2021",
+        endDate: "30/08/2021",
+      },
+    ]
+
+function generetaExperienta(experienta){
+    const exp =document.createElement("div");
+    const name= document.createElement("p");
+    const comp= document.createElement("p");
+    const desc= document.createElement("p");
+    const sd= document.createElement("p");
+    const ed= document.createElement("p");
+    name.innerText=experienta.name;
+    comp.innerText=experienta.company;
+    desc.innerText=experienta.description;
+    sd.innerText=experienta.startDate;
+    ed.innerText=experienta.endDate;
+    exp.appendChild(name);
+    exp.appendChild(comp);
+    exp.appendChild(desc);
+    exp.appendChild(sd);
+    exp.appendChild(ed);
+    return exp;
+}
+function popexp(exp){
+const mainPARENT=document.getElementById("Experienta2");
+exp.forEach(function(expobj ,index){
+    const exper=generetaExperienta(expobj);
+    mainPARENT.appendChild(exper);
+})
+    
+};
+console.log(generetaExperienta(experienta1[0]));
+popexp(experienta1);
---
+++ cv/Studii.js
@@ -0,0 +1,45 @@
+const Studii= [
+    {
+        id: "1",
+        Scoala: "Universitatea Tehnica Cluj Napoca",
+        Specializarea: "Automatica si informatica aplicata",
+        endDate: "2023",
+      },
+    {
+        id: "2",
+        Scoala: "Liceul Teoretic Ana Ipatescu",
+        Specializarea: "Matematica-informatica",
+        endDate: "2019",
+      },
+      {
+        id: "3",
+        Scoala: "Liceul Teoretic Petru Maior",
+        Specializarea: "Stiinte ale naturii",
+        endDate: "2017",
+      }
+    ]
+    function generetaStudii(Studii){
+        const stud =document.createElement("div");
+        const Scoala= document.createElement("p");
+        const Specializarea= document.createElement("p");
+        const endDate= document.createElement("p");
+        Scoala.innerText=Studii.Scoala;
+        endDate.innerText=Studii.endDate;
+        Specializarea.innerText=Studii.Specializarea;
+        stud.appendChild(Scoala);
+        stud.appendChild(Specializarea);
+        stud.appendChild(endDate);
+        return stud;
+    }
+    function popstd(std){
+    const mainPARENT=document.getElementById("Studii");
+    std.forEach(function(stdobj ,index){
+        const stud=generetaStudii(stdobj);
+        mainPARENT.appendChild(stud);
+    })
+        
+    };
+    console.log(generetaStudii(Studii[0]));
+    console.log(generetaStudii(Studii[1]));
+    console.log(generetaStudii(Studii[2]));
+    popstd(Studii);
